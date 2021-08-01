e1f0078f28fa212d592cae70d6629bd54f42b89e
Famille Adouani <familleadouani2019@gmail.com>
2021-08-01 21:49:05 +0200

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7c2b041260358a247e47f6f289079b14'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2cf8563b-c81c-4cc3-8403-0d4b96ceb4b7'
+};
